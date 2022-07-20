c69b65401657f41f5e6cea8060fefb6d94806de0
Ilham AJ <ilhamaryasuta@gmail.com>
2022-07-20 15:24:39 +0700

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.IL2CPP;
+using UnityEngine;
+using HarmonyLib;
+
+namespace RF5NoHat {
+	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	public class Plugin : BasePlugin {
+
+		internal static new ManualLogSource Log;
+		public override void Load() {
+			// Plugin startup logic
+
+			Plugin.Log = base.Log;
+			Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+			Harmony.CreateAndPatchAll(typeof(RF5NoHat));
+		}
+
+		public enum SynthMode {
+			NONE,
+			CRAFT,
+			UPGRADE
+		}
+
+		[HarmonyPatch]
+		public class RF5NoHat {
+			[HarmonyPatch(typeof(HumanModelSocket), nameof(HumanModelSocket.Attach))]
+			[HarmonyPrefix]
+			public static void HideHat(HumanModelSocket __instance) {
+				var attachId = __instance.AttachId;
+				if (attachId is HumanAttachIDEnum.Crown or HumanAttachIDEnum.Fullface or HumanAttachIDEnum.Glasses or HumanAttachIDEnum.Hairpin or HumanAttachIDEnum.Hat or HumanAttachIDEnum.Headband or HumanAttachIDEnum.Ribbon) {
+					Log.LogInfo("Hat Missing?");
+					//visible = false;
+					__instance.IsVisible = false;
+				}
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Rune Factory 5 No Hat
+ A BepInEx mod for Rune Factory 5 that hide head equipments from showing.
+
+## Installation
+- Install this [RF5Fix mod](https://github.com/Lyall/RF5Fix) (BepInEx included there).
+- Download this mod [here.](https://github.com/PurplefinNeptuna/RF5NoHat/releases)
+- Extract RF5NoHat.zip file to `<GameDirectory>\BepInEx\plugins`.
+
+## What this mod do
+- Hide head equipments for your character and also NPCs
+
+## Known issues
+## What to do next (for myself)
